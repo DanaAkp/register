f2f7ae0a2dd0474d8fdf5dc46cc4ea93969a4c21
DanaAkp <aidnagul10@gmail.com>
2021-04-07 19:56:30 +0500

    Готов поиск по названию организации, добавить еще фильтр по ФО Сделать блокировку пользователя

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,9 @@
 from flask import request, url_for, redirect
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.form import SecureForm
-from flask_babelex import Babel
 from flask_login import current_user
-from app.models import User, Organization, ServiceForm, TypeOfService, OrganizationServiceForm, ServiceFormTypeOfService
-import re
 
 
 class OrganizationView(ModelView):
-    # form_base_class = SecureForm
     column_filters = ('name', 'id_form_service')
     column_searchable_list = ('name', 'contacts')
     # column_hide_backrefs = False
